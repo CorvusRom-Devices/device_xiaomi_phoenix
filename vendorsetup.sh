062e8b38c708570db6be544bc77a6ee2a697ce31
rakesh roshan <rakeshraimca@gmail.com>
2020-08-26 13:07:31 +0530

    Phoenix: cleanup and cloning required repo

---
+++ vendorsetup.sh
@@ -0,0 +1,4 @@
+rm -rf frameworks/base
+rm -rf frameworks/av
+git clone https://gitlab.com/rakeshraimca/frameworks_base frameworks/base
+git clone https://gitlab.com/rakeshraimca/framework_av frameworks/av
