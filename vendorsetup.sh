60ce685acb8e135d2c801f2f4b863a8e209414b4
rakesh roshan <rakeshraimca@gmail.com>
2020-08-28 12:16:14 +0530

    Phoenix: adding slow motion fix patch

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,11 @@
-rm -rf frameworks/base
 rm -rf frameworks/av
-git clone https://gitlab.com/rakeshraimca/frameworks_base frameworks/base
 git clone https://gitlab.com/rakeshraimca/framework_av frameworks/av
+
+echo "Patching for slow motion insideframework base..."
+
+ROOTDIR="$PWD"
+CURRENT_DIR="$ROOTDIR/device/xiaomi/phoenix"
+cd $ROOTDIR/frameworks/base
+git am $CURRENT_DIR/slow-motion.patch
+echo "Patching Done"
+cd $ROOTDIR
