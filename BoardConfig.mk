7ce42ec36edb0a4889e7fb0358e8a1a765b8f51f
Trishiraj <gamerprince.exp@gmail.com>
2021-06-30 23:14:18 +0200

    Remove proton call in device
    Cuz we have a flag in source now ;)
    
    Signed-off-by: Trishiraj <gamerprince.exp@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -136,10 +136,6 @@ ifeq ($(TARGET_PREBUILT_KERNEL),)
   TARGET_KERNEL_CONFIG := phoenix_defconfig
   TARGET_KERNEL_CLANG_COMPILE := true
   TARGET_KERNEL_SOURCE := kernel/xiaomi/phoenix
-  KERNEL_SUPPORTS_LLVM_TOOLS := true
-  TARGET_KERNEL_CLANG_VERSION := proton
-  KERNEL_TOOLCHAIN := $(shell pwd)/prebuilts/clang/host/linux-x86/clang-proton/bin
-  TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-gnu-
 endif
 
 TARGET_KERNEL_ADDITIONAL_FLAGS := \
